5d8b43e4f61972ebc07d4c3c16ecd1f5d6e2fdbc
Eliska Ryklova <ryklovae@uni.coventry.ac.uk>
2023-02-10 19:47:43 +0000

    Recipe models added

---
+++ smilecook/Models/RecipeDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smilecook.Models
+{
+    public class RecipeDetails
+    {
+        public string Label { get; set; }
+        public string Url { get; set; }
+        public string Image { get; set; }
+    }
+}
