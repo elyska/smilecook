ba75f7aa070957dea94b0456bddc6146b4812cd8
Eliska Ryklova <ryklovae@uni.coventry.ac.uk>
2023-02-10 19:09:26 +0000

    first commit

---
+++ smilecook/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace smilecook;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ smilecook/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace smilecook;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
