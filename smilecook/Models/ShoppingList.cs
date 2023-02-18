1de094aba0f88a0b234124b83ab9683a854f8b42
Eliska Ryklova <ryklovae@uni.coventry.ac.uk>
2023-02-18 20:52:33 +0000

    Model for shopping list table created

---
+++ smilecook/Models/ShoppingList.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smilecook.Models
+{
+    [Table("shoppingList")]
+    public class ShoppingList
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(250)]
+        public string Ingredient { get; set; }
+    }
+}
