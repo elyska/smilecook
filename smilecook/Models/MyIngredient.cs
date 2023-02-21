4733de8f63dbcaa0a0966a6d262f39adb13a7da1
Eliska Ryklova <ryklovae@uni.coventry.ac.uk>
2023-02-21 12:27:36 +0000

    Models created

---
+++ smilecook/Models/MyIngredient.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smilecook.Models
+{
+    [Table("myIngredients")]
+    public class MyIngredient
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string IngredientLine { get; set; }
+    }
+}
---
+++ smilecook/Models/MyRecipe.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smilecook.Models
+{
+    [Table("myRecipes")]
+    public class MyRecipe
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Label { get; set; }
+        public string Image { get; set; }
+        public string Instructions { get; set; }
+    }
+}
